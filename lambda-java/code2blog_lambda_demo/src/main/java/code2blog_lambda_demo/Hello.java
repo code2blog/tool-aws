0b0790493e824885fbb6eee94c95da95fff2d6b0
vishnu <vishnu31122013@gmail.com>
2020-06-17 19:37:56 +0530

    dr4 body mapping template used to pass http headers to lambda To avoid [Cannot deserialize java.lang.String] error, modify RequestHandler interface to accept object

--- lambda-java/code2blog_lambda_demo/src/main/java/code2blog_lambda_demo/Hello.java
+++ lambda-java/code2blog_lambda_demo/src/main/java/code2blog_lambda_demo/Hello.java
@@ -7,23 +7,21 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.util.Map;
-
-public class Hello implements RequestHandler<Map<String,String>, String> {
+public class Hello implements RequestHandler<Object, String> {
 
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     @Override
-    public String handleRequest(Map<String,String> event, Context context)
+    public String handleRequest(Object event, Context context)
     {
         LambdaLogger logger = context.getLogger();
-        String response = new String("200 OK from java");
+        String response = new String("java lambda says hello");
         // log execution details
-//        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
-//        logger.log("CONTEXT: " + gson.toJson(context));
-//        // process event
-//        logger.log("EVENT: " + gson.toJson(event));
-//        logger.log("EVENT TYPE: " + event.getClass().toString());
+        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+        logger.log("CONTEXT: " + gson.toJson(context));
+        // process event
+        logger.log("EVENT: " + gson.toJson(event));
+        logger.log("EVENT TYPE: " + event.getClass().toString());
         return response;
     }
 }
