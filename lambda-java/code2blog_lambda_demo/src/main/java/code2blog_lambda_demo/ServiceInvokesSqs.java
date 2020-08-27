c9371729cb7e21a196a713b021cef058defb6f65
vishnu <vishnu31122013@gmail.com>
2020-08-27 23:14:34 +0530

    ld6 stream not executing as expected

--- lambda-java/code2blog_lambda_demo/src/main/java/code2blog_lambda_demo/ServiceInvokesSqs.java
+++ lambda-java/code2blog_lambda_demo/src/main/java/code2blog_lambda_demo/ServiceInvokesSqs.java
@@ -4,24 +4,43 @@ package code2blog_lambda_demo;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.MessageAttributeValue;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.util.List;
+import java.util.Optional;
 
 public class ServiceInvokesSqs implements RequestHandler<Object, String> {
 
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     @Override
-    public String handleRequest(Object event, Context context)
-    {
+    public String handleRequest(Object event, Context context) {
         LambdaLogger logger = context.getLogger();
-        String response = new String("java lambda ServiceInvokesSqs says hello");
-        // log execution details
-        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
-        logger.log("CONTEXT: " + gson.toJson(context));
-        // process event
-        logger.log("EVENT: " + gson.toJson(event));
-        logger.log("EVENT TYPE: " + event.getClass().toString());
-        return response;
+        String correlationIdToSearch = "code2blog_corrlationId_123";
+
+        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+        String QUEUE_NAME = "MY_FIRST_QUEUE.fifo";
+        String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+        ReceiveMessageResult receiveMessageResult = sqs.receiveMessage(queueUrl);
+        logger.log("connected to queue");
+        List<Message> messages = receiveMessageResult.getMessages();
+        logger.log("message count : " + messages.size());
+        Optional<Message> matchingMessage = messages.stream().filter(message -> {
+            MessageAttributeValue value = message.getMessageAttributes().get("CorrelationId");
+            if (value == null) return false;
+            logger.log("iterating messages. Found correlation id : " + value);
+            return value.getStringValue().equals(correlationIdToSearch);
+        }).findFirst();
+
+        if (!matchingMessage.isPresent()) {
+            return "message with matching correlation id not found";
+        }
+        return matchingMessage.get().getBody();
     }
 }
