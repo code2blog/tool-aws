ccd21f82f09309046669b5ed832803a575af671f
vishnu <vishnu31122013@gmail.com>
2020-09-30 21:51:58 +0530

    ma-4 soap request sent as string

--- sam-nodejs/code2blog-middleware-api/soap-invoke/app.js
+++ sam-nodejs/code2blog-middleware-api/soap-invoke/app.js
@@ -1,12 +1,28 @@
+const axios = require('axios')
 let response;
 
 exports.lambdaHandler = async (event, context) => {
     try {
+        const sSoapRequest = `<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ndf="https://graphical.weather.gov/xml/DWMLgen/wsdl/ndfdXML.wsdl">
+        <soapenv:Header/>
+        <soapenv:Body>
+           <ndf:LatLonListZipCode soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+              <zipCodeList xsi:type="xsd:string">12345</zipCodeList>
+           </ndf:LatLonListZipCode>
+        </soapenv:Body>
+     </soapenv:Envelope>`;
+        //
+        let sUrl = 'https://graphical.weather.gov/xml/SOAP_server/ndfdXMLserver.php';
+        const soapResponse = await axios.post(sUrl, sSoapRequest, {
+            headers: {
+                'Content-Type': 'text/xml',
+            }});
+
         response = {
             'statusCode': 200,
-            'body': {}
+            'body': soapResponse.data
         };
-        
+
     } catch (err) {
         console.log(err);
         return err;
